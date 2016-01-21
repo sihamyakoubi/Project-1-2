638c23a554f6752fe97a2e7adb5c9f889b65cc19
danny <dannydegraaf16@hotmail.com>
2016-01-21 14:10:09 +0100

    Bla

--- Game/Game/Menu.py
+++ Game/Game/Menu.py
@@ -1,19 +1,16 @@
-﻿import pygame
-
+﻿﻿﻿import pygame
 pygame.init()
 pygame.display.set_caption ("Survivor") # titel van het programma (blauwe balk boven in)
 
-width, height = 800, 600
+width, height = 700, 600
 black = (0,0,0)
 white = (255,255,255)
 red = (255,0,0)
 green = (0,255,0)
 blue = (0,0,255)
 screen = pygame.display.set_mode((width,height)) # scherm
-hand = pygame.image.load("Content/boxing_small.jpg")
-hand2 = pygame.transform.flip(hand,180,0)
-
 time = pygame.time.Clock ()
+
 
 def quitGame():
     pygame.quit()
@@ -40,30 +37,40 @@ def button(msg,x,y,w,h,ic,ac,action=None):
     textRect.center = ( (x+(w/2)), (y+(h/2)) )
     screen.blit(textSurf, textRect)
 
-
 def game_menu():
     menu = True
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 quitGame()
-
         print (event)
         screen.fill(white)
         largeText = pygame.font.SysFont("comicsansms",115)
         TextSurf, TextRect = text_objects("Survivor", largeText)
         TextRect.center = ((width*0.5),(height*0.15))
         screen.blit(TextSurf, TextRect)
-        screen.blit(hand,(width*0.25,height/2))
-        screen.blit(hand2,(width*0.60,height/2))
-        button("Help",width*0.9,height*0.9,50,50,white,black,None)
+
+        
         button("Start",width*0.45,height*0.4,100,50,white,black,None)
         button("Load Game",width*0.45,height*0.5,100,50,white,black,None)
         button("Setting",width*0.45,height*0.6,100,50,white,black,None)
         button("Quit",width*0.45,height*0.7,100,50,white,black,quitGame)
+        button("Help",width*0.9,height*0.9,50,50,white,black,Help_Menu)
 
+        
         pygame.display.update()
         time.tick(60)
+
+def Help_Menu():
+    while True:
+        Help = pygame.image.load("content/Help.png")
+        Help = pygame.transform.scale(Help,(width,height))
+        screen.blit(Help, (0,0 ))
+        button("X",width-20,0,20,20,red,white,game_menu)
+             
+        pygame.display.update()
+        time.tick(60)
+        pygame.display.set_mode((width,height))  
 
 
 
