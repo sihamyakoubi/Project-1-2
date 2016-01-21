bcdc04a4776b9214d9dcb7984f13b8c5b60de109
danny <dannydegraaf16@hotmail.com>
2016-01-21 12:47:26 +0100

    class

---
+++ Game/Game/classpawns.py
@@ -0,0 +1,14 @@
+class Pawns:
+    def __init__(self,pawnPosition,name,colour,fightercard):
+        self.pawnPosition = pawnPosition
+        self.name = name
+        self.colour = colour
+        self.fightercard = fightercard
+
+pawn1 = Pawns(pawnPosition3,"Manny Pecquiao", "Yellow",fightercard3)
+pawn2 = Pawns(pawnPosition2,"Rocky Belboa", "Red",fightercard2)
+pawn3 = Pawns(pawnPosition4,"Badr Heri", "Green",fightercard4)
+pawn4 = Pawns(pawnPosition1,"Mike Tysen", "Blue",fightercard1)
+
+
+
